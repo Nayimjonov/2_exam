bceb8d831248253ac25856ebc83896a5605f3774
Samandar <samandarnayimjonov@gmail.com>
2025-04-17 18:47:43 +0500

    user serialzier done

--- users/serializers.py
+++ users/serializers.py
@@ -18,6 +18,16 @@ class RegisterSerializer(serializers.Serializer):
         return User.objects.create_user(**validated_data)
 
 class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(read_only=True)
+    email = serializers.EmailField(read_only=True)
+    first_name = serializers.CharField(read_only=True)
+    last_name = serializers.CharField(read_only=True)
+    is_teacher = serializers.BooleanField(read_only=True)
+    bio = serializers.CharField(read_only=True)
+    profile_picture = serializers.ImageField(read_only=True)
+    date_joined = serializers.DateTimeField(read_only=True)
 
+    
 
 
