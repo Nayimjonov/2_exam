121cc8186b94ef8c05e9915fa299415853eb66ab
Samandar <samandarnayimjonov@gmail.com>
2025-04-18 10:15:12 +0500

    barcha field lar yozibolindi

--- courses/serializers.py
+++ courses/serializers.py
@@ -7,3 +7,18 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'description', 'icon', 'created_at')
 
 class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = (
+            'is',
+            'title',
+            'description',
+            'teacher',
+            'category',
+            'price',
+            'discount_price',
+            'image',
+            'is_published',
+            'created_at',
+            'updated_at'
+        )
