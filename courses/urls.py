40859c45840f88eb708fabe988f88dfa464d1263
Samandar <samandarnayimjonov@gmail.com>
2025-04-18 10:07:34 +0500

    category detail urls ulandi

--- courses/urls.py
+++ courses/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('categories/', views.CategoryListCreateView.as_view(), name='category-list'),
-
+    path('categories/<int:pk>/', views.CategoryRetrieveUpdateDestroyView.as_view(), name='category-detail'),
 ]
