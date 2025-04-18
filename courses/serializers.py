d25953f15b17a159e5e4cc160b363cbf4317bc22
Samandar <samandarnayimjonov@gmail.com>
2025-04-18 10:12:42 +0500

    course serialzier created

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category, Course
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name', 'description', 'icon', 'created_at')
 
+class CourseSerializer(serializers.ModelSerializer):
