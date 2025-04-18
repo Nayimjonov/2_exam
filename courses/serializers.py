bafba033bb60bf9b6c7194d6a46597fc50935381
Samandar <samandarnayimjonov@gmail.com>
2025-04-18 10:17:15 +0500

    course category serialzie rdone

--- courses/serializers.py
+++ courses/serializers.py
@@ -3,7 +3,8 @@ from .models import Category, Course
 
 
 class CourseCategorySerializer(serializers.Serializer):
-
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(read_only=True)
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
