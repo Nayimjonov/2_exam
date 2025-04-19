ca524997508abfc993ac4faa0934e0a9259d905d
Samandar <samandarnayimjonov@gmail.com>
2025-04-19 11:48:22 +0500

    enrollment list serialzier done

--- enrollments/serializers.py
+++ enrollments/serializers.py
@@ -1,2 +1,28 @@
 from rest_framework import serializers
-from
+from .models import Enrollment
+
+
+class EnrollmentUserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(read_only=True)
+    first_name = serializers.CharField(read_only=True)
+    last_name = serializers.CharField(read_only=True)
+
+
+class EnrollmentCourseSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(read_only=True)
+
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enrollment
+        fields = ('id', 'user', 'course', 'enrolled_at', 'is_completed', 'completed_at')
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['user'] = EnrollmentUserSerializer(instance.user).data
+        representation['course'] = EnrollmentCourseSerializer(instance.course).data
+        return representation
+
+
