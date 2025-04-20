77ba8e230c01aaf61d81b5c914ead55986bf75ab
Samandar <samandarnayimjonov@gmail.com>
2025-04-20 11:40:00 +0500

    review serializer yozildi

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,4 +1,11 @@
 from rest_framework import serializers
+from .models import Review
 
 
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ('id', 'user', 'course', 'rating', 'comment', 'created_at', 'updated_at')
 
